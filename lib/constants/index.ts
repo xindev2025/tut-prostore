7a8453fdc93838e4b67b3537e61a18b13315f7ae
Robert Janagap <janagaprobert@gmail.com>
2025-04-20 13:12:26 +0800

    root layout and constants

---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? 'Prostore'
+export const APP_DESC =
+  process.env.NEXT_PUBLIC_APP_DESC ?? 'Nextjs ecommerce website'
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL ?? 'http://localhost:3000'
