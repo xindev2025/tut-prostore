79943a8cb6d782b2ed52cf0d744810c969320191
Robert Janagap <janagaprobert@gmail.com>
2025-05-04 21:40:38 +0800

    add product type

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,6 +1,6 @@
 import { Pool, neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Product } from '@prisma/client'
 import ws from 'ws'
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
@@ -20,12 +20,12 @@ export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
     product: {
       price: {
-        compute(product) {
+        compute(product: Product) {
           return product.price.toString()
         }
       },
       rating: {
-        compute(product) {
+        compute(product: Product) {
           return product.rating.toString()
         }
       }
