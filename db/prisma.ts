f18e06aff139eabb07606c2645a464767cdb9c93
Robert Janagap <janagaprobert@gmail.com>
2025-05-10 11:20:11 +0800

    remove unused eslint and added product type

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type */
 import { neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Product } from '@prisma/client'
 import ws from 'ws'
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
@@ -21,12 +20,12 @@ export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
     product: {
       price: {
-        compute(product) {
+        compute(product: Product) {
           return product.price?.toString()
         }
       },
       rating: {
-        compute(product) {
+        compute(product: Product) {
           return product.rating?.toString()
         }
       }
