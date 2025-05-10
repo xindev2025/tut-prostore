1a4b8e98171ec9e9fdbf50ba96e2963ea141b69d
Robert Janagap <janagaprobert@gmail.com>
2025-05-10 12:42:25 +0800

    replace prisma path

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,6 +1,6 @@
 import { neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@/lib/generated/prisma'
 import { Decimal } from '@prisma/client/runtime/library'
 import ws from 'ws'
 
