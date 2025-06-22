ea14654705e4340c068d2f4ffb124842106459d6
Robert Janagap <janagaprobert@gmail.com>
2025-06-22 12:40:33 +0800

    create order and capture payment request

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,6 +1,46 @@
 const base = process.env.PAYPAL_API_URL ?? 'https://api-m.sandbox.paypal.com'
 
-export const paypal = {}
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken()
+    const url = `${base}/v2/checkout/orders`
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`
+      },
+      body: JSON.stringify({
+        intent: 'CAPTURE',
+        purchase_units: [
+          {
+            amount: {
+              currency_code: 'USD',
+              value: price
+            }
+          }
+        ]
+      })
+    })
+
+    return handleRequest(response)
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken()
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+
+    return handleRequest(response)
+  }
+}
 
 // generate paypal access token
 async function generateAccessToken() {
@@ -19,13 +59,17 @@ async function generateAccessToken() {
     }
   })
 
+  const jsonData = await handleRequest(response)
+  return jsonData.access_token
+}
+
+// handle request
+async function handleRequest(response: Response) {
   if (response.ok) {
-    const jsonData = await response.json()
-    return jsonData.access_token
+    return response.json()
   } else {
     const errorMessage = await response.text()
     throw new Error(errorMessage)
   }
 }
-
 export { generateAccessToken }
