730d198bd36fee1ba6d267da287459f4b3567e85
Robert Janagap <janagaprobert@gmail.com>
2025-07-09 18:21:52 +0800

    protecting admin routes

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,4 +1,3 @@
-import { auth } from '@/auth'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import {
   Table,
@@ -14,17 +13,15 @@ import { BadgeDollarSign, Barcode, CreditCard, Users } from 'lucide-react'
 import { Metadata } from 'next'
 import Link from 'next/link'
 import Charts from './charts'
+import { requireAdmin } from '@/lib/auth-guard'
 
 export const metadata: Metadata = {
   title: 'Admin Dashboard'
 }
 
 const AdminOverviewPage = async () => {
-  const session = await auth()
-
-  if (session?.user.role !== 'admin') {
-    throw new Error('User is not authorized')
-  }
+  // admin authorization
+  await requireAdmin()
 
   const summary = await getOrderSummary()
   return (
