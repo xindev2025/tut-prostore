b5a12c5b52c93af40bd72911a96a854ad7656af7
Robert Janagap <janagaprobert@gmail.com>
2025-07-27 20:34:19 +0800

    category drawer

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { APP_NAME } from '@/lib/constants'
 import Menu from './menu'
+import CategoryDrawer from './category-drawer'
 
 const Header = () => {
   return (
     <header className='w-full border-b'>
       <div className='wrapper flex-between'>
         <div className='flex-start'>
-          <Link href='/' className='flex-start'>
+          <CategoryDrawer />
+          <Link href='/' className='flex-start ml-4'>
             <Image
               src='/images/logo.svg'
               alt={`${APP_NAME} logo`}
--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -149,3 +149,13 @@ export async function updateProduct(data: z.infer<typeof UpdateProductSchema>) {
     return { success: false, message: formatError(error) }
   }
 }
+
+// get all categories
+export async function getAllCategories() {
+  const data = await prisma.product.groupBy({
+    by: ['category'],
+    _count: true
+  })
+
+  return data
+}
