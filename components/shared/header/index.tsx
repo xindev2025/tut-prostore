92dc92f806d2ee1c2d85a704bcd92a32a0881ac7
Robert Janagap <janagaprobert@gmail.com>
2025-07-28 16:26:58 +0800

    search component

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { APP_NAME } from '@/lib/constants'
 import Menu from './menu'
 import CategoryDrawer from './category-drawer'
+import Search from './search'
 
 const Header = () => {
   return (
@@ -22,6 +23,9 @@ const Header = () => {
               {APP_NAME}
             </span>
           </Link>
+        </div>
+        <div className='hidden md:block'>
+          <Search />
         </div>
         <Menu />
       </div>
