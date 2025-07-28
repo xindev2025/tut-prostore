2bed6716936d0494446d00afdd0ad903a0f683b6
Robert Janagap <janagaprobert@gmail.com>
2025-07-28 21:23:05 +0800

    get filter function

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -20,6 +20,31 @@ const SearchPage = async (props: {
     page = '1'
   } = await props.searchParams
 
+  // url filters
+  const getFilterUrl = ({
+    c,
+    s,
+    p,
+    r,
+    pg
+  }: {
+    c?: string
+    s?: string
+    p?: string
+    r?: string
+    pg?: string
+  }) => {
+    const params: Record<string, string> = {}
+
+    if (c) params.category = c
+    if (p) params.price = p
+    if (s) params.sort = s
+    if (r) params.rating = r
+    if (pg) params.page = pg
+
+    return `/search?${new URLSearchParams(params).toString()}`
+  }
+
   const products = await getAllProducts({
     page: Number(page),
     query: q,
@@ -29,9 +54,10 @@ const SearchPage = async (props: {
     rating,
     sort
   })
+
   return (
     <div className='grid md:grid-cols-5 md:gap-5'>
-      <div className='filter-links'></div>
+      <div className='filter-links'>{/* filters */}</div>
       <div className='md:col-span-4 space-y-4'>
         <div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
           {products.data.length === 0 && <div>No products found</div>}
