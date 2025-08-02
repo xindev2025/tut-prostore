05c5ce9aa46d54c1005383ffcb76ded8b2d5fd05
Robert Janagap <janagaprobert@gmail.com>
2025-08-02 21:00:34 +0800

    review form dialog

--- app/(root)/products/[slug]/review-list.tsx
+++ app/(root)/products/[slug]/review-list.tsx
@@ -3,6 +3,7 @@
 import { Review } from '@/types'
 import Link from 'next/link'
 import { useState } from 'react'
+import ReviewForm from './review-form'
 
 const ReviewList = ({
   userId,
@@ -19,7 +20,7 @@ const ReviewList = ({
     <div className='space-y-4'>
       {reviews.length === 0 && <div>No reviews yet</div>}
       {userId ? (
-        <></>
+        <ReviewForm productId={productId} userId={userId} />
       ) : (
         <div>
           Please
