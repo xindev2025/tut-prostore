c020fe8a1b48441fd0966dc84a90a34979a6bf22
Robert Janagap <janagaprobert@gmail.com>
2025-08-12 20:12:07 +0800

    remove recharts

--- app/admin/overview/charts.tsx
+++ app/admin/overview/charts.tsx
@@ -1,39 +1,41 @@
-'use client'
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+// recharts
+// disable chart because of vercel limit size
+// 'use client'
+// import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
 
-const Charts = ({
-  data: { salesData }
-}: {
-  data: {
-    salesData: { month: string; totalSales: number }[]
-  }
-}) => {
-  return (
-    <ResponsiveContainer width={'100%'} height={350}>
-      <BarChart data={salesData}>
-        <XAxis
-          dataKey={'month'}
-          stroke='#888888'
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-        />
-        <YAxis
-          stroke='#888888'
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-          tickFormatter={(value) => `$${value}`}
-        />
-        <Bar
-          dataKey={'totalSales'}
-          fill='currentColor'
-          radius={[40, 40, 0, 0]}
-          className='fill-primary'
-        />
-      </BarChart>
-    </ResponsiveContainer>
-  )
-}
+// const Charts = ({
+//   data: { salesData }
+// }: {
+//   data: {
+//     salesData: { month: string; totalSales: number }[]
+//   }
+// }) => {
+//   return (
+//     <ResponsiveContainer width={'100%'} height={350}>
+//       <BarChart data={salesData}>
+//         <XAxis
+//           dataKey={'month'}
+//           stroke='#888888'
+//           fontSize={12}
+//           tickLine={false}
+//           axisLine={false}
+//         />
+//         <YAxis
+//           stroke='#888888'
+//           fontSize={12}
+//           tickLine={false}
+//           axisLine={false}
+//           tickFormatter={(value) => `$${value}`}
+//         />
+//         <Bar
+//           dataKey={'totalSales'}
+//           fill='currentColor'
+//           radius={[40, 40, 0, 0]}
+//           className='fill-primary'
+//         />
+//       </BarChart>
+//     </ResponsiveContainer>
+//   )
+// }
 
-export default Charts
+// export default Charts
